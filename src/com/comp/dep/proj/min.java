49543a568e058281e1b1fa438caca272428a2df7
sgithub <swathiajay1@gmail.com>
2020-09-19 09:40:47 -0700

    Minimum Value

--- src/com/comp/dep/proj/min.java
+++ src/com/comp/dep/proj/min.java
@@ -7,21 +7,18 @@ public class min {
         Integer ina[]={4,2,1,3};
         int firstNumber=450;
         float fistFloat=400f;
-        System.out.println(firstNumber);
-        System.out.println(fistFloat);
-//        if(abc[])4>=4
-        for(int i=0;i<=ina.length-1;i++){
-            int firstLoopNum=ina[i];
-           // System.out.println("Number:"  +ina[i]);
-            for(int j=0;j<=ina.length-1;j++){
-                int secondLoopNum=ina[j];
-//                1<2
-                if(firstLoopNum < secondLoopNum){
-                    //4,3
-                    System.out.println("index:"+i+" Value:"+ina[i]);
-                }
-//                System.out.println("index:"+i+" Value:"+ina[i]);
+        int i;
+        int min=ina[0];
+        for(i=1;i<ina.length;i++){
+            if (ina[i] < min){
+                min = ina[i];
             }
         }
+        System.out.println("Mini -Value:" +min);
+
+
+        System.out.println(firstNumber);
+        System.out.println(fistFloat);
+
     }
 }
